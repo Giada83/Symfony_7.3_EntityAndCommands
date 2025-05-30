70ea7f9e138c0d821a0a375edac525ec7835bb45
Giada Petrillo <157986302+Giada83@users.noreply.github.com>
2025-05-30 20:01:19 +0200

    Aggiunta entità 'Article' con relazione Many2One (e conseguente aggiunta del campo One2Many in 'Author')

---
+++ src/Entity/Article.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    // FIELDS
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null; // Id
+
+    #[ORM\Column(length: 70)]
+    #[Assert\NotBlank(message: 'Title is required')]
+    #[Assert\Length(
+        min: 30,
+        max: 70,
+        minMessage: 'The title must be at least {{ limit }} characters long',
+        maxMessage: 'The title cannot be longer than {{ limit }} characters'
+    )]
+    private ?string $title = null; // title
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null; // content
+
+    #[ORM\Column]
+    private ?\DateTime $published_at = null; // published_at
+
+    //MANY2ONE RELATIONSHIP
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Author $author = null;
+
+    // GETTER AND SETTER
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTime
+    {
+        return $this->published_at;
+    }
+
+    public function setPublishedAt(\DateTime $published_at): static
+    {
+        $this->published_at = $published_at;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AuthorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 #validazioni
 use Symfony\Component\Validator\Constraints as Assert;
@@ -10,14 +12,16 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
 #[UniqueEntity('email')] //unique applicato al campo email
+
 class Author
 {
+    // FIELDS
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $id = null;
+    private ?int $id = null; // id
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 50)]
     // validazioni
     #[Assert\NotBlank(message: 'The name of the author cannot be empty')]
     #[Assert\Length(
@@ -26,7 +30,7 @@ class Author
         minMessage: 'The name of the author must be at least {{ limit }} characters long',
         maxMessage: 'The name of the author cannot be longer than {{ limit }} characters',
     )]
-    private ?string $name = null;
+    private ?string $name = null; //name
 
     #[ORM\Column(length: 255)]
     //validazioni
@@ -34,8 +38,21 @@ class Author
         message: 'The email {{ value }} is not a valid email.',
     )]
     #[Assert\NotBlank(message: 'Email is required')]
-    private ?string $email = null;
+    private ?string $email = null; //email
 
+    //ONE2MANY RELATIONSHIP
+    /**
+     * @var Collection<int, Article>
+     */
+    #[ORM\OneToMany(targetEntity: Article::class, mappedBy: 'author')]
+    private Collection $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    // GETTER AND SETTER
     public function getId(): ?int
     {
         return $this->id;
@@ -61,6 +78,36 @@ class Author
     public function setEmail(string $email): static
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Article>
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): static
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles->add($article);
+            $article->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): static
+    {
+        if ($this->articles->removeElement($article)) {
+            // set the owning side to null (unless already changed)
+            if ($article->getAuthor() === $this) {
+                $article->setAuthor(null);
+            }
+        }
 
         return $this;
     }
