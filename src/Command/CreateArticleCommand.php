e73bea9ba54c16b8788bcdb6b63f2c912b297fc1
Giada Petrillo <157986302+Giada83@users.noreply.github.com>
2025-05-31 17:31:28 +0200

    Console Command per creare Articoli con selezione dell'autore tramite id

---
+++ src/Command/CreateArticleCommand.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Author;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+#[AsCommand(
+    name: 'app:create-article',
+    description: 'Creates a new article',
+    hidden: false,
+)]
+
+class CreateArticleCommand extends Command
+{
+    public function __construct(private EntityManagerInterface $em,)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Creates a new article')
+            ->setHelp('Demonstration of custom commands created by Symfony Console component.');;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $helper = $this->getHelper('question');
+        $questionTitle = new Question('Insert title(required) : ');
+        $title = $helper->ask($input, $output, $questionTitle);
+
+        $questionContent = new Question('Write your article : ');
+        $content = $helper->ask($input, $output, $questionContent);
+
+        //selection autori
+        $output->writeln([
+            '',
+            ' ----------- ',
+            ' Author List ',
+            ' ----------- ',
+            ''
+        ]);
+
+        $authors = $this->em->getRepository(Author::class)->findAll();
+        if (count($authors) > 0) {
+            foreach ($authors as $a) {
+                $line = str_pad($a->getName(), 20) . '| id: ' . $a->getId();
+                $choices[] = $line;
+                $output->writeln($line);
+            }
+        }
+        //inserimento valore campo autore_id
+        $questionAuthor = new Question('<comment>Insert the \'id\' of the author: </comment>');
+        $author_id = $helper->ask($input, $output, $questionAuthor);
+        $author = $this->em->getRepository(Author::class)->find($author_id);
+        if (!$author) {
+            $output->writeln('<error>Author not found</error>');
+            return Command::FAILURE;
+        }
+
+        // nuovo ogetto Article
+        $article = new Article();
+        $article->setTitle($title);
+        $article->setContent($content);
+        $article->setPublishedAt(new \DateTime());
+        $article->setAuthor($author);
+
+        // salvataggio nel db
+        $this->em->persist($article);
+        $this->em->flush();
+
+        // messaggio di successo
+        $output->writeln('Saved new article with id: ' . $article->getId());
+
+        return Command::SUCCESS;
+    }
+}
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,30 +10,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 final class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'create_article')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $author = new Author();
-        $author->setName('author' . rand(1, 1000));
-        $author->setEmail('author' . rand(1, 1000) . '@example.com');
+        // $author = new Author();
+        // $author->setName('author' . rand(1, 1000));
+        // $author->setEmail('author' . rand(1, 1000) . '@example.com');
 
         $article = new Article();
         $article->setTitle('title' . rand(1, 1000));
         $article->setContent('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
         $article->setPublishedAt(new \DateTime());
 
-        // relates this article to the author
-        $article->setAuthor($author);
+        // associa l'articolo a un autore (id generato in maniera randomica con valore di default)
+        $repository = $entityManager->getRepository((Author::class));
+        $author = $repository->findAll();
+        $randomIndex = rand(1, 10);
+        $random_author = $author[$randomIndex] ?? $author[0];
+        $article->setAuthor($random_author);
 
-        $entityManager->persist($author);
+        // $entityManager->persist($author);
         $entityManager->persist($article);
         $entityManager->flush();
 
         return new Response(
-            'Saved new article with id: ' . $article->getId()
-                . ' and new author with id: ' . $author->getId()
+            'Saved new article with id: ' . $article->getId() . ', written by ' . $random_author->getName()
         );
     }
 }
